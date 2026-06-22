ee148dc9ab11340e8e07249a29d47929c426cd3d
Buda Adrian <budaadrian94@gmail.com>
2026-06-23 00:08:58 +0300

    Add GitHub Actions workflows for build, test, PR verification and publishing (#8)
    * Add GitHub Actions workflows for build, test, PR verification and publishing.
    
    * Add upload/download artifact steps to ensure image availability between jobs.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
-VERSION = "0.0.1"
+MAJOR_VERSION = 0
+MINOR_VERSION = 0
+PATCH_VERSION = 2
 
 def get_version():
-    return VERSION
+    return f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"
 
 if __name__ == "__main__":
     print("Starting Hivebox v " + get_version())
